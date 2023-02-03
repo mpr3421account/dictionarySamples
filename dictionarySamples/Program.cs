7bcf1895e587b6fe2858dc7a4fadbe909b3cc1fe
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-03 16:26:11 -0300

    FInal

--- dictionarySamples/Program.cs
+++ dictionarySamples/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, string> cookies = new Dictionary<string, string>();
+cookies["user"] = "maria";
+cookies["email"] = "maria@gmail.com";
+cookies["phone"] = "99771122";
+cookies["phone"] = "99771133";
+Console.WriteLine(cookies["email"]);
+cookies.Remove("email");
+Console.WriteLine("Phone number: " + cookies["phone"]);
+if (cookies.ContainsKey("email"))
+{
+    Console.WriteLine("Email: " + cookies["email"]);
+}
+else
+{
+    Console.WriteLine("There is not 'email' key");
+}
+Console.WriteLine("Size: " + cookies.Count);
+Console.WriteLine("ALL COOKIES:");
+foreach (KeyValuePair<string, string> item in cookies)
+{
+    Console.WriteLine(item.Key + ": " + item.Value);
+}
